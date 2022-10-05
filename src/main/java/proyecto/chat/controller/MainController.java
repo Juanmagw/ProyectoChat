d488ee9b7d183a4a37a0a224edf5ed7c791025ee
Juanmagw <juanma4x21@gmail.com>
2022-10-05 13:23:43 +0200

    Terminando de asociar codigo a parte grafica

--- src/main/java/proyecto/chat/controller/MainController.java
+++ src/main/java/proyecto/chat/controller/MainController.java
@@ -1,41 +1,31 @@
 package proyecto.chat.controller;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import proyecto.chat.App;
-
-import java.io.IOException;
 
 public class MainController {
 
-    @FXML private Button btnMinimizar;
-    @FXML private Button btnMaximizar;
-    @FXML private Button btnCerrar;
-    @FXML private Button btnSportChat;
-    @FXML private Button btnCookChat;
-    @FXML private Button btnVideogamesChat;
-    @FXML private Button btnMusicChat;
+    @FXML private Button btnMinimize;
+    @FXML private Button btnMaximize;
+    @FXML private Button btnClose;
+    @FXML private Button btnChat;
     @FXML private Button btnHelp;
     @FXML private Button btnRules;
 
-    @FXML public void minimizar(){
-        btnMinimizar.setOnMouseClicked(minimizar->{
-            ((Stage)((Button)minimizar.getSource()).getScene().getWindow()).setIconified(true);
+    @FXML public void minimize(){
+        btnMinimize.setOnMouseClicked(minimize->{
+            ((Stage)((Button)minimize.getSource()).getScene().getWindow()).setIconified(true);
         });
     }
-    @FXML public void cerrar(){
-        btnCerrar.setOnMouseClicked(cerrar->{
+    @FXML public void close(){
+        btnClose.setOnMouseClicked(close->{
             System.exit(0);
         });
     }
 
-    @FXML public void chatPage() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("chatPage.fxml"));
-        Stage stage = (Stage) btnSportChat.getScene().getWindow();
-        stage.setScene(new Scene(fxmlLoader.load(), 900, 450));
+    @FXML public void chatPage() throws Exception {
+        App.setRoot("chatPage");
     }
 }
