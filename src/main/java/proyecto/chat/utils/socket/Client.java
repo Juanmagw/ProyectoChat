236e4e03212a16abb5e8b8fe1583432fe920ba34
Juanmagw <juanma4x21@gmail.com>
2022-10-09 18:56:49 +0200

    nada2

---
+++ src/main/java/proyecto/chat/utils/socket/Client.java
@@ -0,0 +1,76 @@
+package proyecto.chat.utils.socket;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) {
+        if(args.length!=2) {
+            System.out.println("Usage: java Client ipServer folderToBeTransfered");
+        }else {
+            String ipServer=args[0];
+            String folderToBeTransfered=args[1];
+
+            Socket socket=null;
+            InputStream in=null;
+            ObjectOutputStream out=null;
+
+            try {
+                socket=new Socket(ipServer,8080);
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return;
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return;
+            }
+
+            try {
+                File folder=new File(folderToBeTransfered);
+                if(folder.exists() && folder.isDirectory()) {
+                    out=new ObjectOutputStream(socket.getOutputStream());
+                    System.out.println("Client: creando stream de salida");
+
+                    String[] files = folder.list();
+                    System.out.println("Client: Manejando "+files.length+" archivos");
+                    for(String file : files) {
+                        File f=new File(folderToBeTransfered+"/"+file);
+                        if(f.exists() && f.isFile()) {
+                            FileInputStream ff=new FileInputStream(f);
+                            byte[] data=ff.readAllBytes();
+                            FileToBeTransfered ftbt = new FileToBeTransfered(f.getName(), data.length);
+                            ftbt.setFileData(data);
+                            out.writeObject(ftbt);
+                            out.flush();
+                            System.out.println("Client: archivo transferido con éxito");
+                        }
+                    }
+
+
+
+                }else {
+                    System.out.println("Client: La carpeta no existe");
+                    return;
+                }
+
+                out.close();
+                //in.close();
+                socket.close();
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/proyecto/chat/utils/socket/Server.java
@@ -0,0 +1,66 @@
+package proyecto.chat.utils.socket;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalTime;
+
+public class Server {
+    public static void main(String[] args) throws ClassNotFoundException {
+        ServerSocket serverSocket = null;
+        boolean serverListening=false;
+
+        try {
+            serverSocket=new ServerSocket(8080);
+            serverListening=true;
+        }catch (IOException e) {
+            System.out.println("Server: No se puede ejecutar el servidor en el puerto 8080");
+        }
+
+        Socket socket=null;
+        ObjectInputStream in=null;
+        OutputStream out=null;
+
+        if(serverListening) {
+            try {
+                System.out.println("Server: Listo para recibir peticiones");
+                socket=serverSocket.accept();
+
+                String name=LocalTime.now().toString();
+
+                File folder=new File(name);
+                if(folder.mkdir()) {
+                    in=new ObjectInputStream(socket.getInputStream());
+                    do {
+                        FileToBeTransfered ftbt = (FileToBeTransfered)in.readObject();
+
+                        out=new BufferedOutputStream(new FileOutputStream(name+"/"+ftbt.getFilename()));
+                        out.write(ftbt.getFileData(), 0, ftbt.getFileSize());
+                        out.close();
+                    }while(socket.isConnected());
+
+                    in.close();
+                }else {
+                    System.out.println("Server: no se puede crear la carpeta destino");
+                }
+
+
+                socket.close();
+                serverSocket.close();
+                System.out.println("Server: Cerrando Servidor");
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+
+
+
+        }
+    }
+}
